bc96ddae9daf565fd975b3d06061ef71812911ea
1106883 <1106883@rgu.ac.uk>
2016-03-03 12:06:33 +0000

    Rejected commit: Default

--- GroupWork/test.php
+++ GroupWork/test.php
@@ -19,7 +19,7 @@ error_reporting(-1);
     }
 
     $query = "";
-    $title = 'Xillia'; //$_POST['title'];
+    $title = $_POST['.title.'];
 
     //if (isset($title)) {//if keyword set goes here
         $query = "SELECT * FROM gameCollection WHERE Title LIKE '%$title%'";
