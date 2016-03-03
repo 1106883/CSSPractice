59d901be50acfe9ab0470727fbd0c6f3aca5fa59
1106883 <1106883@rgu.ac.uk>
2016-03-03 14:08:12 +0000

    Rejected commit: Default

--- GroupWork/test.php
+++ GroupWork/test.php
@@ -48,7 +48,7 @@ else {
 }
 */
 
-        $query = "SELECT * FROM gameCollection WHERE Title LIKE '%$title%' AND Platform Like '$platform' AND Genre Like '$genre' AND Year Like '$year'";
+        $query = "SELECT * FROM gameCollection WHERE Title LIKE '%$title%' AND Platform Like '$platform' AND Genre Like '$genre' AND Year = '$year'";
         try {
             $results = $conn->query($query);
 
