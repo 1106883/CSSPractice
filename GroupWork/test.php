af0c085b60ddf28b6f70b5c9ad70c9d1218420e9
1106883 <1106883@rgu.ac.uk>
2016-03-07 12:35:33 +0000

    Rejected commit: Default

--- GroupWork/test.php
+++ GroupWork/test.php
@@ -78,7 +78,7 @@
                     } else {
 
                         print "<table id='results'>\n";
-                        echo "<th>Title</th><th>Platform</th><th>Genre</th><th>Year</th><th width = 10%>Age Rating</th><th>Description</th>";
+                        echo "<th>Title</th><th>Platform</th><th>Genre</th><th>Year</th><th id='age'>Age Rating</th><th id='desc'>Description</th>";
                         foreach ($results as $row) {
                             echo "<tr>";
                             echo "<td>" . $row["Title"] . "</td>";
