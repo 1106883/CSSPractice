7982050e1affbd6a277711b4ec0cb948368fcfc1
1106883 <1106883@rgu.ac.uk>
2016-03-07 09:55:27 +0000

    Rejected commit: Default

--- PHP/wantedgood.php
+++ PHP/wantedgood.php
@@ -10,12 +10,16 @@ $wantedgood = "specs";
 switch($wantedgood){
     case "mugs":
         echo "You must be 16 to buy mugs";
+        break;
     case "specs":
         echo "You must be 18 to buy specs";
+        break;
     case "sausage rolls":
         echo "You must be 16 to buy sausage roll";
+        break;
     default:
         echo "You're a sucky suck";
+        break;
 }
 
 ?>
