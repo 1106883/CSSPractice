4397bdc782820451422266f3e104ea2ce9a88c64
1106883 <1106883@rgu.ac.uk>
2016-03-07 09:46:42 +0000

    Rejected commit: Default

---
+++ PHP/myage.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1106883
+ * Date: 07/03/2016
+ * Time: 09:41
+ */
+$myage = 18;
+$canbuy = "I can buy";
+
+echo "I am " + $myage;
+
+if($myage >= 16) {
+    $canbuy += "specs";
+    if($myage >= 18) {
+        $canbuy += ", mugs";
+        if($myage >= 21) {
+            $canbuy += "and sausage rolls";
+        }
+    }
+}
+?>
