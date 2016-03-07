6909a75313d671f92343af5731493e08f6960540
1106883 <1106883@rgu.ac.uk>
2016-03-07 10:18:52 +0000

    Rejected commit: Default

---
+++ PHP/forloop.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1106883
+ * Date: 07/03/2016
+ * Time: 10:04
+ */
+for ($i = 1; $i < 30; $i++)
+{
+    echo "On the $i you can buy ";
+    if (($i % 2) == 0){
+        echo "specs";
+    }
+    if (($i % 3) == 0){
+        echo "mugs";
+    }
+    if (($i % 4) == 0){
+        echo "sausage rolls";
+    }
+}
+?>
