84d342c21d7c23350ea4839213956ad66b77a590
1106883 <1106883@rgu.ac.uk>
2016-03-07 09:59:19 +0000

    Rejected commit: Default

---
+++ PHP/array.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1106883
+ * Date: 07/03/2016
+ * Time: 09:57
+ */
+
+$myArrray = array("specs", "drugs", "rock n roll");
+
+foreach($provisionedActivity as $x) {
+    print "<p>$x</p>";
+}
+?>
