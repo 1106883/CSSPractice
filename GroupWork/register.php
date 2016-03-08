29d4b7bf4182ebe38c3da24c7777be0d873f8b27
1106883 <1106883@rgu.ac.uk>
2016-03-08 10:38:30 +0000

    Rejected commit: Default

---
+++ GroupWork/register.php
@@ -0,0 +1,94 @@
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="design.css"/>
+</head>
+
+<body>
+<div id="wrapper">
+    <div id="header">
+        <div id="logo">
+            <h1><strong><font size="20">GameShare RGU</font></strong></h1>
+        </div>
+        <div id="search">
+            <Form Name ="login">
+                <div id="logintext">
+                    <font size="-2"><label for="username">Username :</label><input id="Username" name="username" type="text" size="-2"/><label for="Password">Password :</label><input id="Password" name="password" type="password" size="-2"/></font><input class="form-submit" type="submit" value="Login" />
+                    <a id="register" href="Register.html">Not a member? Register.</a>
+                </div>
+            </form>
+        </div>
+        <div id="menu">
+            <form action="results.php" method="post">
+                <ul>
+                    <li><a href="#">Homepage</a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="search.html">Search</a></li>
+                    <li><input id="qsearch" name="qsearch" type="text" placeholder="I want to borrow..."/><input id="qsgo" type="submit"  value="Go"></li>
+                </ul>
+            </form>
+            <br class="clearfix" />
+        </div>
+    </div>
+    <div id="page">
+        <div id="content">
+
+                <?php
+
+                //header("Location:results.html");
+
+                error_reporting(-1);
+
+                $dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
+                $username = "b52b6c6935c6d2";
+                $password = "26ebeed0";
+                try {
+                    $conn = new PDO($dsn, $username, $password);
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                } catch (PDOException $e) {
+                    echo "Connection failed: " . $e->getMessage();
+                }
+
+                $query = "";
+
+                $fname = $_POST['Fname'];
+
+                $sname = $_POST['Sname'];
+
+                $tuno = $_POST['stuno'];
+
+                $email = $_POST['email'];
+                $Cemail = $_POST['Cemail'];
+                if ($email = $Cemail) {
+                    $email = $_POST['email'];
+                }
+                else $email = "";
+
+                $password = $_POST['epassword'];
+                $Cpassword = $_POST['Cpassword'];
+                if ($password = $Cpassword) {
+                    $password = $_POST['password'];
+                }
+                else $password = "";
+
+                $query = "INSERT INTO members(firstName, lastName, email, studentID, password)
+                          VALUES ('$fname', '$sname', '$email', '$stuno', '$password')";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "New record created successfully";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            ?>
+        </div>
+        <br class="clearfix" />
+    </div>
+    <div id="footer">
+        &copy; 2016. All rights reserved. Design by <strong>GROUP C</strong>.
+    </div>
+</div>
+</body>
+</html>
+
+
