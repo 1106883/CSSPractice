20c88abd96a56b7aafd1487aa1c01c2ff05cc993
1106883 <1106883@rgu.ac.uk>
2016-03-10 11:41:05 +0000

    Rejected commit: Default

--- GroupWork/login.php
+++ GroupWork/login.php
@@ -53,7 +53,7 @@
                 $password = $_POST['password'];
 
 
-                $sql = "SELECT * FROM members WHERE studentID = $stuno AND password LIKE '$password'";
+                $sql = "SELECT * FROM members WHERE studentID = $stuno";
 
                 try {
                     $results = $conn->query($query);
